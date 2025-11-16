ff3d8f1a81f3ae154cd88f36c0629c1c32f14a2f
UwU_Snek <xiaoju345@gmail.com>
2025-11-16 17:48:48 +0100

    Define custom initialization phase Fix initialization order

--- src/main/java/com/snek/frameworklib/FrameworkLib.java
+++ src/main/java/com/snek/frameworklib/FrameworkLib.java
@@ -3,12 +3,14 @@ package com.snek.frameworklib;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import com.snek.frameworkconfig.FrameworkConfig;
 import com.snek.frameworklib.configs.Configs;
 import com.snek.frameworklib.input.MessageReceiver;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 
 
@@ -19,7 +21,8 @@ import net.minecraft.server.MinecraftServer;
 
 
 public class FrameworkLib implements ModInitializer {
-    public static final String LIB_ID = "framework-lib";
+    public static final String LIB_ID = "frameworklib";
+    public static final ResourceLocation INIT_PHASE_ID = new ResourceLocation(LIB_ID, "init");
 
 
     // Server instance
@@ -36,22 +39,30 @@ public class FrameworkLib implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        System.out.println("FrameworkLib loaded :3");
 
 
-        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
+        // Make sure Framework Config is initialized before Framework Lib
+        ServerLifecycleEvents.SERVER_STARTING.addPhaseOrdering(
+            FrameworkConfig.INIT_PHASE_ID,
+            INIT_PHASE_ID
+        );
+
+
+        // Register initialization
+        ServerLifecycleEvents.SERVER_STARTING.register(INIT_PHASE_ID, server -> {
             serverInstance = server;
 
             // Read config files
             Configs.loadConfigs();
-
-
-
 
             // Register chat input handler
             ServerMessageEvents.ALLOW_CHAT_MESSAGE.register((message, sender, params) -> {
                 return !MessageReceiver.onMessage(message, sender);
             });
         });
+
+
+        // Log library loading
+        System.out.println("FrameworkLib loaded :3");
     }
 }
