109a94b074f84195da21e058e3777c72c439951c
UwU_Snek <thatsnekuwu@gmail.com>
2025-11-25 04:32:14 +0100

    Code cleanup

--- src/main/java/com/snek/frameworklib/mixin/UnwearableHeadItemMixin.java
+++ src/main/java/com/snek/frameworklib/mixin/UnwearableHeadItemMixin.java
@@ -4,19 +4,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.snek.frameworklib.utils.MinecraftUtils;
 
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.Equipable;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.PlayerHeadItem;
-import net.minecraft.world.level.gameevent.GameEvent;
 
 
 
