143baeb7528b26d50dde162ab533116fc9a2756f
UwU_Snek <thatsnekuwu@gmail.com>
2025-12-14 03:45:16 +0100

    Remove redundant UNHOVERED_W constant from TextInputElmStyle

--- src/main/java/com/snek/frameworklib/graphics/functional/styles/TextInputElmStyle.java
+++ src/main/java/com/snek/frameworklib/graphics/functional/styles/TextInputElmStyle.java
@@ -18,7 +18,6 @@ import com.snek.frameworklib.utils.Easings;
  * The style of the generic TextInputElm UI element.
  */
 public class TextInputElmStyle extends FancyButtonElmStyle {
-    public static final float UNHOVERED_W = 0.05f;
 
 
     /**
@@ -33,21 +32,21 @@ public class TextInputElmStyle extends FancyButtonElmStyle {
     public @Nullable Animation getDefaultHoverPrimerAnimation() {
         return new Animation(
             new Transition()
-            .additiveTransformBg(new Transform().scaleX(UNHOVERED_W))
+            .additiveTransformBg(new Transform().scaleX(__base_ButtonElmStyle.HIDDEN_W))
         );
     }
     @Override
     public @Nullable Animation getDefaultHoverEnterAnimation () {
         return new Animation(
             new Transition(__base_ButtonElmStyle.HOVER_ANIMATION_TIME, Easings.expOut)
-            .additiveTransformBg(new Transform().scaleX(1f / UNHOVERED_W))
+            .additiveTransformBg(new Transform().scaleX(1f / __base_ButtonElmStyle.HIDDEN_W))
         );
     }
     @Override
     public @Nullable Animation getDefaultHoverLeaveAnimation () {
         return new Animation(
             new Transition(__base_ButtonElmStyle.HOVER_ANIMATION_TIME, Easings.expOut)
-            .additiveTransformBg(new Transform().scaleX(UNHOVERED_W))
+            .additiveTransformBg(new Transform().scaleX(__base_ButtonElmStyle.HIDDEN_W))
         );
     }
 }
